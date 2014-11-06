3dd48518ff98cfcca509f9c030477c858b919f80
sdickreuter <Simon.Dickreuter@uni-tuebingen.de>
2014-11-06 17:35:14 +0100

    added methods for getting button/analogstick state

--- gamepad.py
+++ gamepad.py
@@ -21,8 +21,8 @@ class Gamepad(object):
             for dev in devs:
                 if dev.idVendor == 0x046d and dev.idProduct == 0xc21d:
                     d = dev
-        conf = d.configurations[0]
-        intf = conf.interfaces[0][0]
+        #conf = d.configurations[0]
+        #intf = conf.interfaces[0][0]
         self._dev = d.open()
         try:
             self._dev.detachKernelDriver(0)
@@ -31,6 +31,9 @@ class Gamepad(object):
         #handle.interruptWrite(0, 'W')
         self._dev.claimInterface(0)
 
+        #This value has to be send to the gamepad, or it won't start working
+        # value was determined by sniffing the usb traffic with wireshark
+        # getting other gamepads to work might be a simple as changing this
         self._dev.interruptWrite(0x02,struct.pack('<BBB', 0x01,0x03,0x04))
 
         self._state = multiprocessing.Array('i', [0,]*20 )
@@ -45,29 +48,59 @@ class Gamepad(object):
         running = True
         data = None
         while running:
+            self.changed.value = 0
             try:
-                data = self._dev.interruptRead(0x81,0x20,500)
-                self._state = struct.unpack('<'+'B'*20, data)
-                print(self._state)
-                self.changed = 1
+                data = self._dev.interruptRead(0x81,0x20,2000)
+                data = struct.unpack('<'+'B'*20, data)
+                for i in range(20):
+                    self._state[i] = data[i]
+                #print(self._state)
+                self.changed.value = 1
             except usb.core.USBError:
                 data = None
-                self.changed = 0
             #connection.send(data)
             #running = connection.recv()
         return True
+
+    def get_LB(self):
+        return self._state[3] == 1
+
+    def get_RB(self):
+        return self._state[3] == 2
+
+    def get_A(self):
+        return self._state[3] == 16
+
+    def get_B(self):
+        return self._state[3] == 32
+
+    def get_X(self):
+        return self._state[3] == 64
+
+    def get_Y(self):
+        return self._state[3] == 128
+
+    def get_analogL_x(self):
+        return self._state[11]
+
+    def get_analogL_y(self):
+        return self._state[12]
+
+    def get_analogR_x(self):
+        return self._state[6]
+
+    def get_analogR_y(self):
+        return self._state[8]
 
     def __del__(self):
         self._worker.join(0.2)
         self._dev.releaseInterface()
         self._dev.reset()
 
-
-
-
 # Unit test code
 if __name__ == '__main__':
    pad = Gamepad()
    while True:
-      if pad.changed == 1:
-        print(pad._state[:])
+      if pad.changed.value == 1:
+        #print(pad._state[:])
+        print("analog R: {0:3}|{1:3}  analog L: {2:3}|{3:3}".format(pad.get_analogR_x(),pad.get_analogR_y(),pad.get_analogL_x(),pad.get_analogL_y()))
